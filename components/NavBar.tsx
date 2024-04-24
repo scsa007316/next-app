da67deb582d4df59d78b04f418c5747eec9a4f70
Aadarsh singh <bond007spectre316@gmail.com>
2024-04-24 11:10:28 +0530

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -14,16 +14,17 @@ const NavBar = () => {
       style={{ backgroundColor: 'rgba(74, 112, 194, 0.75)', padding: '1rem', zIndex: 2 }}
       className="flex justify-between items-center"
     >
-     <a
-  style={{ fontSize: '3rem', fontWeight: 'bold', color: 'white', textDecoration: 'none' }}
-  className="hover:underline"
+    <a
   href="https://scsa007316.github.io/"
+  className="text-3xl font-bold text-white hover:underline"
+  style={{fontSize: '3rem', fontWeight: 'bold', color: 'white', textDecoration: 'none' , marginRight: '50%' }} // Use marginRight: 'auto' to push to the right
 >
   Aadarsh Singh
 </a>
 
 
-      <div className="space-x-10 flex items-center">
+
+     
         <a className="text-white hover:underline rounded-full p-1" href="/ML/Physics">
           Theoretical Physics
         </a>
@@ -39,33 +40,34 @@ const NavBar = () => {
           Animation
         </button>
         <a
-          href="https://www.linkedin.com/in/aadarsh-singh-506454260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-2"
-        >
-          {/* LinkedIn icon */}
-          <img
-            src="/Logos/Linkedin_logo.png" // Replace with the actual path to your LinkedIn icon/image
-            alt="LinkedIn"
-            className="w-6 h-6"
-          />
-        </a>
+  href="https://www.linkedin.com/in/aadarsh-singh-506454260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="ml-2"
+>
+  {/* LinkedIn icon */}
+  <img
+    src="/Logos/Linkedin_logo.png" // Replace with the actual path to your LinkedIn icon/image
+    alt="LinkedIn"
+    style={{ width: '4rem', height: '4rem' }} // Adjust the width and height as needed
+  />
+</a>
+
         
-        <a
-          href="https://github.com/scsa007316"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-2"
-        >
-          {/* GitHub icon */}
-          <img
-            src="/Logos/github-mark-white.png" // Replace with the actual path to your GitHub icon/image
-            alt="GitHub"
-            className="w-6 h-6"
-          />
-        </a>
-      </div>
+<a
+  href="https://github.com/scsa007316"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="ml-2"
+>
+  {/* GitHub icon */}
+  <img
+    src="/Logos/github-mark-white.png" // Replace with the actual path to your GitHub icon/image
+    alt="GitHub"
+    style={{ width: '4rem', height: '4rem' }} // Adjust the width and height as needed
+  />
+</a>
+
     </nav>
   );
 };
